0cd2236f7781a1202b6c7357eb25c96d6df95de8
Максим <oma489@gmail.com>
2024-07-21 02:14:06 +0500

    Фильтр таблицы по маршруту

---
+++ app/controllers/technique_filter.php
@@ -0,0 +1,48 @@
+<table class="table table-bordered mt-4">
+    <tr>
+        <th>Гос номер</th>
+        <th>Модель</th>
+        <th>Тип</th>
+    </tr>
+    <?php
+    include_once 'E:/repository/portal_vkazym/portal_vkazym/app/database/dbFunction.php';
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $join = [
+            'INNER JOIN type_of_technique ON technique.id_type_of_techniques = type_of_technique.id',
+        ];
+        $free_technique = selectAllJoinRes('technique', [], joins: $join);
+        if (!empty($free_technique)) {
+            // Вывод данных таблицы
+            foreach ($free_technique as $row) {
+                echo "<tr>";
+                echo "<td>{$row['state_number']}</td>";
+                echo "<td>{$row['name_technique']}</td>";
+                echo "<td>{$row['name']}</td>";
+                echo "</tr>";
+
+            }
+        } else {
+            echo "<tr><td colspan='100%'>No data found</td></tr>";
+        }
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' & isset($_POST['id_technique'])) {
+        $join = [
+            'INNER JOIN type_of_technique ON technique.id_type_of_techniques = type_of_technique.id',
+        ];
+        $id = $_POST['id_technique'];
+        $params = ['id' => $id, 'faulty' => 0];
+        $free_technique = selectAllJoinRes('technique', $params, joins: $join);
+        if (!empty($free_technique)) {
+            foreach ($free_technique as $row) {
+                echo "<tr>";
+                echo "<td>{$row['state_number']}</td>";
+                echo "<td>{$row['name_technique']}</td>";
+                echo "<td>{$row['name']}</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='3'>No data found</td></tr>";
+        }
+    }
+    ?>

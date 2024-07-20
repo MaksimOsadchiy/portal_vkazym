f1a2ca497274dfb627dec31fdc254f4d25274018
Emmanuil <emmanuil.litvinenko@inbox.ru>
2024-07-20 22:21:19 +0500

    refactor(placeOrder.js): comment on the code, correct the logic of the conditions

--- assets/js/addTechnique/placeOrder.js
+++ assets/js/addTechnique/placeOrder.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-	//
+	/**
+	 * Асинхронная функция, которая отправляет запрос на сервер для создания нового заказа.
+	 *
+	 * Функция выполняет следующие шаги:
+	 * 1. Вызывает функцию `collectContent` для сбора данных заказа, которые будут отправлены на сервер.
+	 * 2. Отправляет HTTP POST запрос на сервер по адресу `orders.php` с данными заказа в формате JSON.
+	 * 3. Ожидает получения ответа от сервера и преобразует его в формат JSON.
+	 * 4. Проверяет, успешен ли ответ сервера (статус ответа `ok`). Если нет, выбрасывает ошибку с кодом статуса.
+	 * 5. Если запрос успешен, создаёт и диспатчит событие `updateError` с сообщением об успешной отправке заказа.
+	 * 6. В случае ошибки во время запроса или обработки ответа, создаёт и диспатчит событие `updateError` с деталями ошибки.
+	 *
+	 * @throws {Error} Если запрос не удался или произошла ошибка при обработке ответа, будет выброшена ошибка с кодом статуса или сообщением об ошибке.
+	 */
 	const createOrder = async () => {
 		try {
-			const request = getContent(); // Формируем тело запроса
+			const request = collectContent(); // Формируем тело запроса
 			const response = await fetch(`${SERVER_URL}orders.php`, {
 				method: 'POST',
 				headers: {
@@ -11,20 +23,20 @@ document.addEventListener('DOMContentLoaded', () => {
 				body: JSON.stringify(request),
 			});
 			const jsonResponse = await response.json(); // Получаем тело ответа
-			if (!response.ok) {
-				throw new Error(jsonResponse.status);
-			}
+			if (!response.ok) throw new Error(jsonResponse.status); // Проверяем HTTP статус ответа
+
 			document.dispatchEvent(
 				new CustomEvent('updateError', {
 					detail: 'Заказ отправлен на рассмотрение!',
 				})
 			);
 		} catch (error) {
+			// Если была ошибка, то обновляем переменную
 			document.dispatchEvent(new CustomEvent('updateError', { detail: error.message }));
 		}
 	};
 	//
-	const getContent = () => {
+	const collectContent = () => {
 		const service = SESSION['service'];
 		const technique = Array.from(document.querySelectorAll('.technique-select'))
 			.map((elem) => elem.value)
@@ -39,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		let timeTo = document.querySelector('.time-to').value;
 		const shift = Array.from(document.querySelectorAll('.form-check-input')).find((elem) => elem.checked)?.value;
 
-		// Обработчик времени
+		// Обработчик времени доделать...
 		// const shiftFrom = new Date(`${dateFrom}T${shift.slice(0, 5)}`);
 		// const shiftTo = +shift[0] ? new Date(`${dateTo}T${shift.slice(0, 5)}`) : new Date(`${dateFrom}T${shift.slice(6, 11)}`);
 		// const fullDateFrom = new Date(`${dateFrom}T${timeFrom}`);
@@ -53,68 +65,59 @@ document.addEventListener('DOMContentLoaded', () => {
 		// 	flag ? console.log('Верно') : console.log('Не верно');;
 		// };
 
-		let flag = '';
-		if (service) {
-			// переписать либо на switch либо придумать логику
-			if (technique.length) {
-				if (responsiblePerson.length) {
-					if (dateFrom && dateTo) {
-						if (route) {
-							if ((timeFrom && timeTo) || shift) {
-								timeFrom = timeFrom ? timeFrom : shift.slice(0, 4);
-								timeTo = timeTo ? timeTo : shift.slice(6, 11);
-								const combinedArray = [];
-								technique.forEach((item, index) => {
-									if (item && responsiblePerson[index]) {
-										combinedArray.push([item, responsiblePerson[index]]);
-									}
-								});
-								const result = [];
-								combinedArray.forEach((subArr) => {
-									const obj = {
-										service_id: +service,
-										technique_id: +subArr[0],
-										route_id: +route,
-										responsible_person_id: +subArr[1],
-										date_from: dateFrom,
-										date_to: dateTo,
-										time_from: timeFrom + ':00',
-										time_to: timeTo + ':00',
-									};
-									shift && (obj.shift = +shift[0] ? 1 : 0);
-									result.push(obj);
-								});
-								return result;
-							} else {
-								flag = 'Введите время начала и окончания работ или выберите смену!';
-							}
-						} else {
-							flag = 'Выберите маршрут!';
-						}
-					} else {
-						flag = 'Введите дату начала и окончания работ!';
-					}
-				} else {
-					flag = 'Обязательное поле: Ответственный!';
-				}
-			} else {
-				flag = 'Обязательное поле: Техника!';
+		validateInputs(service, technique, responsiblePerson, dateFrom, dateTo, route, timeFrom, timeTo, shift);
+		timeFrom = timeFrom ? timeFrom : shift.slice(0, 4);
+		timeTo = timeTo ? timeTo : shift.slice(6, 11);
+		const combinedArray = [];
+		technique.forEach((item, index) => {
+			if (item && responsiblePerson[index]) {
+				combinedArray.push([item, responsiblePerson[index]]);
 			}
-		} else {
-			flag = 'В сессии нет ID сервиса!';
-		}
+		});
+		const result = [];
+		combinedArray.forEach((subArr) => {
+			const obj = {
+				service_id: +service,
+				technique_id: +subArr[0],
+				route_id: +route,
+				responsible_person_id: +subArr[1],
+				date_from: dateFrom,
+				date_to: dateTo,
+				time_from: timeFrom + ':00',
+				time_to: timeTo + ':00',
+			};
+			shift && (obj.shift = +shift[0] ? 1 : 0);
+			result.push(obj);
+		});
+		return result;
+	};
+	//
+	const validateInputs = (service, technique, responsiblePerson, dateFrom, dateTo, route, timeFrom, timeTo, shift) => {
+		const errors = {
+			noService: 'В сессии нет ID сервиса!',
+			noTechnique: 'Обязательное поле: Техника!',
+			noResponsiblePerson: 'Обязательное поле: Ответственный!',
+			noDate: 'Введите дату начала и окончания работ!',
+			noRoute: 'Выберите маршрут!',
+			noTimeOrShift: 'Введите время начала и окончания работ или выберите смену!',
+		};
 
-		if (flag) {
-			throw new Error(flag);
-		}
+		if (!service) throw new Error(errors.noService);
+		if (!technique.length) throw new Error(errors.noTechnique);
+		if (!responsiblePerson.length) throw new Error(errors.noResponsiblePerson);
+		if (!dateFrom || !dateTo) throw new Error(errors.noDate);
+		if (!(timeFrom || shift) || !(timeTo || shift)) throw new Error(errors.noTimeOrShift);
+		if (!route) throw new Error(errors.noRoute);
+
+		return true;
 	};
 	//
 	const addEventCreateOrder = () => {
 		const btnCreateOrder = document.querySelector('.create-order');
-		// btnCreateOrder.addEventListener('click', () => getContent());
+		// btnCreateOrder.addEventListener('click', () => collectContent());
 		btnCreateOrder.addEventListener('click', async () => await createOrder());
 	};
 
-	// Начало скрипта
+	// Основной блок кода, который выполняет начальные операции при загрузке скрипта.
 	addEventCreateOrder();
 });

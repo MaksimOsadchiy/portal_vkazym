25cd0a2fdaea32ea84731e4b96585fd0ceb529e9
Emmanuil <emmanuil.litvinenko@inbox.ru>
2024-08-11 22:03:57 +0500

    refactor: delete old files and change the database

--- app/controllers/technique_back.php
+++
@@ -1,7 +0,0 @@
-<?php
-// include('app/database/dbFunction.php'); // вроде как смысла нет 
-
-//Код для формы заказа техники
-if ($_SERVER['REQUEST_METHOD'] === 'POST' & isset($_POST['order_technique'])) {
-
-}
